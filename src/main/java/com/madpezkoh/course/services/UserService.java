90cfb7608b78bcbb9144d061b3f8fb339da8c74f
Felipe Gregorio <f.gregorio@outlook.com>
2021-04-19 22:01:28 -0300

    Service Layer, Component Registration

---
+++ src/main/java/com/madpezkoh/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.madpezkoh.course.services;
+
+import com.madpezkoh.course.entities.User;
+import com.madpezkoh.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
